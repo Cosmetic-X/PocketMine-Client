a88858065ef41a794e8697d178a952703d3530b1
xxAROX <jansohn@hurensohn.me>
2021-12-12 14:20:19 +0100

    Updated build.php

--- build.php
+++ build.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-ini_set("phar.readonly", "0");
+ini_set("phar.readonly", "1");
 var_dump(ini_get("phar.readonly"));
 /**
  * Build script
@@ -10,22 +10,27 @@ const FILE_NAME = "Cosmetic-X";
 const COMPRESS_FILES = true;
 const COMPRESSION = Phar::GZ;
 $startTime = microtime(true);
+
 // Input & Output directory...
 $from = getcwd() . DIRECTORY_SEPARATOR;
 $to = getcwd() . DIRECTORY_SEPARATOR . "out" . DIRECTORY_SEPARATOR . FILE_NAME . DIRECTORY_SEPARATOR;
 @mkdir($to, 0777, true);
+
 // Clean output directory...
 cleanDirectory($to);
+
 // Copying new files...
 copyDirectory($from . "src", $to . "src");
 copyDirectory($from . "resources", $to . "resources");
 $description = yaml_parse_file($from . "plugin.yml");
 yaml_emit_file($to . "plugin.yml", $description);
+
 // Defining output path...
 $outputPath = CUSTOM_OUTPUT_PATH == ""
 	? getcwd() . DIRECTORY_SEPARATOR . "out" . DIRECTORY_SEPARATOR . FILE_NAME . "_{$description["version"]}.phar"
 	: CUSTOM_OUTPUT_PATH;
 @unlink($outputPath);
+
 // Generate phar
 $phar = new Phar($outputPath);
 $phar->buildFromDirectory($to);
@@ -33,6 +38,7 @@ if (COMPRESS_FILES) {
 	$phar->compressFiles(COMPRESSION);
 }
 printf("Plugin built in %s seconds! Output path: %s\n", round(microtime(true) - $startTime, 3), $outputPath);
+
 function copyDirectory(string $from, string $to): void{
 	mkdir($to, 0777, true);
 	$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($from, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::SELF_FIRST);
