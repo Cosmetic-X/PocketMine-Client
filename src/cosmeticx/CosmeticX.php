a57eda2deffe14eaf1adf522322aa020a43b8b7c
xxAROX <arox.email.0@gmail.com>
2022-02-02 04:56:06 +0100

    permission-ing for operators

--- src/cosmeticx/CosmeticX.php
+++ src/cosmeticx/CosmeticX.php
@@ -11,6 +11,7 @@ use cosmeticx\command\CosmeticXCommand;
 use cosmeticx\task\async\SendRequestAsyncTask;
 use Frago9876543210\EasyForms\elements\Image;
 use Phar;
+use pocketmine\permission\DefaultPermissions;
 use pocketmine\permission\Permission;
 use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\PluginBase;
@@ -168,10 +169,12 @@ class CosmeticX extends PluginBase{
 	private function registerPermissions(): void{
 		unset($this->permissions);
 		$this->permissions = [];
+		$op = PermissionManager::getInstance()->getPermission(DefaultPermissions::ROOT_OPERATOR);
 		$overlord = new Permission("cosmetic-x.*", "Overlord permission");
 		foreach ($this->command->getSubCommands() as $subCommand) {
 			if (!is_null($subCommand->getPermission())) {
 				$permission = $this->command->getPermission() . "." . $subCommand->getPermission();
+				$op->addChild($permission, true);
 				PermissionManager::getInstance()->addPermission($this->permissions[] = new Permission($permission, "Allows to use the '/{$this->command->getName()} {$subCommand->getName()}' command."));
 				$overlord->addChild($permission, true);
 			}
