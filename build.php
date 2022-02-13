3d68266027efecb545db194266b3109695a0c99c
xxAROX <arox.email.0@gmail.com>
2022-02-13 14:31:26 +0100

    fixed spam listeners

--- build.php
+++ build.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 /**
  * Build script
  */
-$IS_GITHUB_ACTIONS = (getenv("IS_GITHUB_ACTIONS") !== false);
 $startTime = microtime(true);
 // Input & Output directory...
 $from = getcwd() . DIRECTORY_SEPARATOR;
@@ -35,28 +34,6 @@ if (is_dir($from . "resources")) {
 yaml_emit_file($to . "plugin.yml", $description);
 // Defining output path...
 @unlink($outputPath . ".phar");
-if($IS_GITHUB_ACTIONS){
-	if (file_exists($API = $from . "out" . DIRECTORY_SEPARATOR . ".API.txt")) {
-		unlink($API);
-	}
-	if (file_exists($VERSION = $from . "out" . DIRECTORY_SEPARATOR . ".VERSION.txt")) {
-		unlink($VERSION);
-	}
-	if (file_exists($FULL_VERSION = $from . "out" . DIRECTORY_SEPARATOR . ".FULL_VERSION.txt")) {
-		unlink($FULL_VERSION);
-	}
-	if (file_exists($FILE_NAME = $from . "out" . DIRECTORY_SEPARATOR . ".FILE_NAME.txt")) {
-		unlink($FILE_NAME);
-	}
-	if (file_exists($FOLDER = $from . "out" . DIRECTORY_SEPARATOR . ".FOLDER.txt")) {
-		unlink($FOLDER);
-	}
-	file_put_contents($API, (is_array($description["api"]) ? explode(".", $description["api"][0])[0] : (is_string($description["api"]) ? explode(".", $description["api"])[0] : "???")), 0777);
-	file_put_contents($FULL_VERSION, $description["version"], 0777);
-	file_put_contents($VERSION, explode("+", $description["version"])[0], 0777);
-	file_put_contents($FILE_NAME, $outputPath, 0777);
-	file_put_contents($FOLDER, $to, 0777);
-}
 // Generate phar
 $phar = new Phar($outputPath . ".phar");
 $phar->buildFromDirectory($to);
