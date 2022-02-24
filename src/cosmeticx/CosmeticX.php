edc755454199b879b97edbd6a41da179569a8d05
xxAROX <arox.email.0@gmail.com>
2022-02-24 15:10:21 +0100

    updated copyright

--- src/cosmeticx/CosmeticX.php
+++ src/cosmeticx/CosmeticX.php
@@ -2,7 +2,13 @@
 /*
  * Copyright (c) Jan Sohn
  * All rights reserved.
- * This plugin is under GPL license
+ * Only people with the explicit permission from Jan Sohn are allowed to modify, share or distribute this code.
+ *
+ * You are NOT allowed to do any kind of modification to this plugin.
+ * You are NOT allowed to share this plugin with others without the explicit permission from Jan Sohn.
+ * You are NOT allowed to run this plugin on your server as source code.
+ * You MUST acquire this plugin from official sources.
+ * You MAY run this plugin on your server as .phar file.
  */
 
 namespace cosmeticx;
